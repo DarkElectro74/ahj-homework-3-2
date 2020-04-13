1af8283025214df9accbdf9b43993be8c4766b99
DarkElectro <electrodark74@gmail.com>
2020-04-13 13:37:03 +0500

    ahj-hw-3-2

---
+++ README.md
@@ -0,0 +1,3 @@
+# ahj-homeworks-3-2
+https://DarkElectro74.github.io/ahj-homework-3-2/
+[![Build status](https://ci.appveyor.com/api/projects/status/ninecsw3nlh156h3?svg=true)](https://ci.appveyor.com/project/DarkElectro/ahj-homework-3-2)
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+/* eslint-disable class-methods-use-this */
+// import ArrData from './ArrData.js';
+// import DisplayForm from './DisplayForm.js';
+import WorkTasks from './WorkTasks.js';
+
+const workTasks = new WorkTasks();
+workTasks.init();
